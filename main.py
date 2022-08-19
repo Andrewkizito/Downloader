189c5b2400d126c7c058c4003d52c069ab929149
AndrewKizito <AndrewKizito54@gmail.com>
2022-08-19 03:55:23 +0300

    Improved

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    "http://www.todaytvseries1.com/tv-series/612-evil-cbs")
+
+if response.status_code == 200:
+    data = response.content.decode()
+    soup = BeautifulSoup(data, "html.parser")
+    urls = []
+
+    for a in soup.find_all('a', href=True):
+        if "EVL10" in a['href']:
+            urls.append(a['href'])
+
+    urls.reverse()
+    portion = urls[0:10]
+    print(portion)
+
+    for key in portion:
+        target_file = key.replace(
+            "http://tirnaice2th.parsaspace.com/un5gjct7qe8cgqwuecgiecohovq/", "")
+
+        try:
+            os.system(f"wget {key}")
+        except Exception as e:
+            print(e)
+            break
