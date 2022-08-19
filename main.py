79aff7b0af0d6036820ba6065f8a3a730c971bf2
AndrewKizito <AndrewKizito54@gmail.com>
2022-08-19 03:59:06 +0300

    Improved

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if response.status_code == 200:
             urls.append(a['href'])
 
     urls.reverse()
-    portion = urls[0:10]
+    portion = urls[0:20]
     print(portion)
 
     for key in portion:
