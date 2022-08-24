73d49a7a04db2d8d13346f60a9c28fe38d760d6b
AndrewKizito <AndrewKizito54@gmail.com>
2022-08-24 08:32:20 +0300

    Improved

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ response = requests.get(
 
 if response.status_code == 200:
     data = response.content.decode()
-    soup = BeautifulSoup(data, "html.parser")
+    text = data[20545:23487]
+    soup = BeautifulSoup(text, "html.parser")
     urls = []
 
     for a in soup.find_all('a', href=True):
-        if "Btr.C.S.S04E" in a['href']:
-            urls.append(a['href'])
+        urls.append(a['href'])
 
     urls.reverse()
     portion = urls[0:10]
     print(portion)
 
-    os.system(f"mkdir bcs && cd bcs")
-    for key in portion:
-        target_file = key.replace(
-            "http://transfer1.parsaspace.com/yvhkvh0q2g7uohuklvh8oplfvbyigv79q2goehkgv7oug/", "")
-        try:
-            os.system(f"wget {key}")
-        except Exception as e:
-            print(e)
-            break
+    # os.system(f"mkdir bcs && cd bcs")
+    # for key in portion:
+    #     target_file = key.replace(
+    #         "http://transfer1.parsaspace.com/yvhkvh0q2g7uohuklvh8oplfvbyigv79q2goehkgv7oug/", "")
+    #     try:
+    #         os.system(f"wget {key}")
+    #     except Exception as e:
+    #         print(e)
+    #         break
