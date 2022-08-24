90977e1b9d19b16c6ed15a29b1694fde6ee0df66
AndrewKizito <AndrewKizito54@gmail.com>
2022-08-24 09:18:38 +0300

    Improved

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get(
-    "http://www.todaytvseries1.com/tv-series/210-better-call-saul-tv1")
+    "http://www.todaytvseries1.com/tv-series/323-stranger-things-netflix-tv")
 
 if response.status_code == 200:
     data = response.content.decode()
-    text = data[20545:23487]
-    soup = BeautifulSoup(text, "html.parser")
+    soup = BeautifulSoup(data, "html.parser")
     urls = []
 
     for a in soup.find_all('a', href=True):
-        urls.append(a['href'])
+        if "S01E0" in a['href']:
+            urls.append(a['href'])
 
     urls.reverse()
-    portion = urls[0:10]
+    portion = urls[0:4]
+    print(portion)
 
-    os.system(f"mkdir bcs && cd bcs")
     for key in portion:
-        target_file = key.replace(
-            "http://transfer1.parsaspace.com/yvhkvh0q2g7uohuklvh8oplfvbyigv79q2goehkgv7oug/", "")
         try:
             os.system(f"wget {key}")
         except Exception as e:
