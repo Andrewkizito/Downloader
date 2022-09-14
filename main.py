2d37472bcc9642b13df0a1ae237dc617efdccf71
AndrewKizito <AndrewKizito54@gmail.com>
2022-09-14 11:28:42 +0300

    Improved

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-# response = requests.get(
-#     "http://www.todaytvseries1.com/tv-series/685-house-of-the-dragon-hbo-max")
+response = requests.get(
+    "http://www.todaytvseries1.com/tv-series/685-house-of-the-dragon-hbo-max")
 
-# if response.status_code == 200:
-#     data = response.content.decode()
-#     soup = BeautifulSoup(data, "html.parser")
-urls = ["https://s5.uupload.ir/files/todaytv/ungvujsgdiuvnslkdnvibsluidhjnvbwyifbonkfjsngdfji/Hus.of.t.Drgn.S01E01.rar",
-        "https://s5.uupload.ir/files/todaytv/ungvujsgdiuvnslkdnvibsluidhjnvbwyifbonkfjsngdfji/Hus.of.t.Drgn.S01E02.rar", "https://s5.uupload.ir/files/todaytv/ungvujsgdiuvnslkdnvibsluidhjnvbwyifbonkfjsngdfji/Hus.of.t.Drgn.S01E03.rar"]
+if response.status_code == 200:
+    data = response.content.decode()
+    soup = BeautifulSoup(data, "html.parser")
 
-# for a in soup.find_all('a', href=True):
-#     if "S04E0" in a['href']:
-#         urls.append(a['href'])
+urls = []
+for a in soup.find_all('a', href=True):
+    if "S04E0" in a['href']:
+        urls.append(a['href'])
 
-# urls.reverse()
-# portion = urls[0:9]
-# print(portion)
+        urls.reverse()
+        portion = urls[0:9]
 
-os.system("mkdir season-2")
-os.system("cd season-2")
+os.system("mkdir files")
+os.system("cd files")
 
 for key in urls:
     os.system(f"wget {key}")
---
+++ readme.md
@@ -0,0 +1,2 @@
+## My Downloader
+This is just a python script to download a bunch of videos
