b91afb27baa2646e19c91a3c0b077b44af3619e2
AndrewKizito <AndrewKizito54@gmail.com>
2022-11-14 21:11:29 +0300

    Improved

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get(
-    "http://www.todaytvseries2.com/tv-series/190-colony-tv1")
+    "http://www.todaytvseries2.com/tv-series/188-the-100-tv1")
 
 if response.status_code == 200:
     data = response.content.decode()
@@ -12,13 +12,13 @@ if response.status_code == 200:
 
 urls = []
 for a in soup.find_all('a', href=True):
-    if "S05" in a['href'] or "Cly.S01E0" in a['href']:
+    if "S05" in a['href'] or "100.S01E0" in a['href']:
         urls.append(a['href'])
 
 print(urls)
 
-os.system("mkdir ~/colony")
-os.system("cd ~/colony")
+os.system("mkdir ~/the-100")
+os.system("cd ~/the-100")
 
 for key in urls:
     os.system(f"wget {key}")
