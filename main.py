15e48d023092d9f27ba41617395a6f834162cbac
AndrewKizito <AndrewKizito54@gmail.com>
2022-12-24 13:01:27 +0300

    Improved

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get(
-    "http://www.todaytvseries2.com/tv-series/85-the-walking-dead-tv")
+    "http://www.todaytvseries2.com/tv-series/85-the-walking-dead-tv"
+)
 
 if response.status_code == 200:
     data = response.content.decode()
     soup = BeautifulSoup(data, "html.parser")
 
 urls = []
-for a in soup.find_all('a', href=True):
-    if "The.Wlk.Ded.S03E" in a['href']:
-        urls.append(a['href'])
+for a in soup.find_all("a", href=True):
+    if "The.Wlk.Ded.S03E" in a["href"]:
+        urls.append(a["href"])
 
 urls.reverse()
-keys = urls[:6]
+keys = urls[6:12]
 
 for key in keys:
     os.system(f"wget {key}")
